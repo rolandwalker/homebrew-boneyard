1d939b721f2dec5afaf897073ed5ce3715680df7
Roland Walker <walker@pobox.com>
2014-11-15 11:57:07 -0500

    add deleted Cask repast

---
+++ Casks/repast.rb
@@ -0,0 +1,8 @@
+# Fri May 31 16:33:07 2013 +0800 https://github.com/caskroom/homebrew-cask/commit/4bb0733 Add anki cask [ouyang]
+cask :v1 => 'repast' do
+  url 'http://jaist.dl.sourceforge.net/project/repast/Repast%20Simphony/Repast%20Simphony%202.0/Repast%20Simphony%202.0%2032.dmg'
+  homepage 'http://repast.sourceforge.net/'
+  version '3.7.1'
+  sha256 'f0189769f54ec627685f8b90478ff427e49321fb'
+  app 'Repast-Simphony-2.0/Repast Simphony.app'
+end
