c90c60757f0f474f0a7f88dbde65dfa07708cd47
Roland Walker <walker@pobox.com>
2014-11-15 11:57:07 -0500

    add deleted Cask musicmanager

---
+++ Casks/musicmanager.rb
@@ -0,0 +1,8 @@
+# Thu May 8 15:05:05 2014 -0700 https://github.com/caskroom/homebrew-cask/commit/7c53279 Upgrade Google's Music Manager.app to 1.0.104.6528 [ninjabong]
+cask :v1 => 'musicmanager' do
+  url 'https://dl.google.com/dl/androidjumper/mac/718015/musicmanager.dmg'
+  homepage 'https://music.google.com'
+  version '1.0.71.8015'
+  sha256 '16f93db5f672313fb3da66b3abbaf4408a136cc677a00851d5cf7221d76c25a5'
+  app 'MusicManager.app'
+end
