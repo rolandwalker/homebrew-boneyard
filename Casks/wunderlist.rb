a81cdfb29cab10fff698c60c9a875ef88ddf008f
Roland Walker <walker@pobox.com>
2014-11-15 11:57:07 -0500

    add deleted Cask wunderlist

---
+++ Casks/wunderlist.rb
@@ -0,0 +1,9 @@
+# coding: utf-8
+# Sat Apr 5 17:54:03 2014 +0100 https://github.com/caskroom/homebrew-cask/commit/3773bd4 removed wunderlist [Vítor Galvão]
+cask :v1 => 'wunderlist' do
+  url 'https://www.macupdate.com/download/35862/Wunderlist.zip'
+  homepage 'https://www.wunderlist.com/en/'
+  version '2.3.2'
+  sha256 'd7adf0cb3b9ab0cd4265ccc7c1e757cd38e74dbcf8c56017baf443dd2abec1b8'
+  app 'Wunderlist.app'
+end
