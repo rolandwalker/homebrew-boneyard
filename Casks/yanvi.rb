b4aaff2d07634b21673b9b26f31222c0e874420e
Roland Walker <walker@pobox.com>
2014-11-15 11:57:07 -0500

    add deleted Cask yanvi

---
+++ Casks/yanvi.rb
@@ -0,0 +1,9 @@
+# coding: utf-8
+# Sat Apr 12 21:02:18 2014 +0100 https://github.com/caskroom/homebrew-cask/commit/4378dd3 Remove YANVI [Vítor Galvão]
+cask :v1 => 'yanvi' do
+  url 'http://download1167.mediafire.com/2ve2xmcd85ug/iy2s1yp233pzj80/Yet_Another_NFO_Viewer_v1.0.2.dmg'
+  homepage 'https://www.macupdate.com/app/mac/39748/yet-another-nfo-viewer'
+  version '1.0.2'
+  sha256 '7e7db828e404d1594ca847c4ccbb4fe7924dd5d3a116e69ddef94884345749ed'
+  app 'YaNvi.app'
+end
