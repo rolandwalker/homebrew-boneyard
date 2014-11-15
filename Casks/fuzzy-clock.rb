6ef6f678653586fe9dee7d05366d9395462d199f
Roland Walker <walker@pobox.com>
2014-11-15 11:57:07 -0500

    add deleted Cask fuzzy-clock

---
+++ Casks/fuzzy-clock.rb
@@ -0,0 +1,8 @@
+# Fri Jun 28 22:06:56 2013 +0200 https://github.com/caskroom/homebrew-cask/commit/5692792 Removes fuzzy-clock [Robert Curth]
+cask :v1 => 'fuzzy-clock' do
+  url 'http://www.objectpark.org/Releases/FuzzyClock-1-2-10.dmg'
+  homepage 'http://www.objectpark.org/FuzzyClock.html'
+  version '1.2.10'
+  sha256 'f4d49e5819d7af020f202e7561b15ae203be357b'
+  app 'FuzzyClock.app'
+end
