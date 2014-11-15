c129a0614e3028561eec0de62a498e9c850095e8
Roland Walker <walker@pobox.com>
2014-11-15 11:57:07 -0500

    add deleted Cask textual

---
+++ Casks/textual.rb
@@ -0,0 +1,8 @@
+# Sat Sep 21 19:17:11 2013 +0200 https://github.com/caskroom/homebrew-cask/commit/6269d23 Remove textual demo [Adam Stankiewicz]
+cask :v1 => 'textual' do
+  url 'http://www.codeux.com/textual/private/downloads/builds/Textual_Demo_12536.zip'
+  homepage 'http://www.codeux.com/textual/'
+  version '2.1.1'
+  sha256 'af057d11e3578afa3b80f4f26093978389982ca2'
+  app 'Textual.app'
+end
