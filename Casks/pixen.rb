7908bbe5e3c9b5949955ca26b347eb767b07944b
Roland Walker <walker@pobox.com>
2014-11-15 11:57:07 -0500

    add deleted Cask pixen

---
+++ Casks/pixen.rb
@@ -0,0 +1,8 @@
+# Tue Oct 8 09:15:16 2013 -0700 https://github.com/caskroom/homebrew-cask/commit/23c45b0 Remove Pixen [Fernando Paredes]
+cask :v1 => 'pixen' do
+  url 'http://www.mattrajca.com/assets/files/Pixen_3_5_10b1.zip'
+  homepage 'http://pixenapp.com/'
+  version '3.5.10b1'
+  sha256 'a2a0f9be3e658abbe551db2250cc0d152977d2c1'
+  app 'Pixen.app'
+end
