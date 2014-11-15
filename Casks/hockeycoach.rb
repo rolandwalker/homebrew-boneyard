cbe31d34ffde9e1c3e31a669a8d750a70ba16ac6
Roland Walker <walker@pobox.com>
2014-11-15 11:57:07 -0500

    add deleted Cask hockeycoach

---
+++ Casks/hockeycoach.rb
@@ -0,0 +1,8 @@
+# Thu Mar 13 16:40:50 2014 -0700 https://github.com/caskroom/homebrew-cask/commit/e008f64 Remove Hockeycoach [Fernando Paredes]
+cask :v1 => 'hockeycoach' do
+  url 'http://download.hockeyapp.net/apps/HockeyCoach/HockeyCoach-1.0.2.zip'
+  homepage 'http://hockeyapp.net/releases/hockeycoach/'
+  version '1.0.2'
+  sha256 '423ef6c54dfe21f9fe6a34d6a1daafa86d6bcd9d26c89e0262112b2a9c78c893'
+  app 'HockeyCoach.app'
+end
