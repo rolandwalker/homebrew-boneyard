1b5ae123106099589ff98ee9327dd2ca7acd8ff8
Roland Walker <walker@pobox.com>
2014-11-15 11:57:07 -0500

    add deleted Cask echofon

---
+++ Casks/echofon.rb
@@ -0,0 +1,7 @@
+# coding: utf-8
+# Sat Dec 8 16:49:38 2012 +1300 https://github.com/caskroom/homebrew-cask/commit/4bd1543 Echofon is discontinued [Félix Saparelli]
+cask :v1 => 'echofon' do
+  url 'http://file.echofon.com/twitter/mac/bin/Echofon_1.5.8.dmg'
+  homepage 'http://www.echofon.com/'
+  version '1.5.8'
+end
