cc490654e63e1810eb1cdd136543564b67f9c313
Roland Walker <walker@pobox.com>
2014-11-15 11:57:07 -0500

    add deleted Cask reeder

---
+++ Casks/reeder.rb
@@ -0,0 +1,8 @@
+# Tue Jun 17 18:41:20 2014 +0800 https://github.com/caskroom/homebrew-cask/commit/ef7887c Delete reeder.rb [Zili Yin]
+cask :v1 => 'reeder' do
+  url 'http://reederapp.com/beta-reeder-for-mac/Reeder2.0b7.zip'
+  homepage 'http://reederapp.com/mac/'
+  version '2.0b7'
+  sha256 '6778fe2747d907e153666c01b3ee8700ed31b05e128c8433fb24f429a62dfdd2'
+  app 'Reeder.app'
+end
