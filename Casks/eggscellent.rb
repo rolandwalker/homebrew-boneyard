be57cd7db5b8878416f2dee4e78d9866734366b9
Roland Walker <walker@pobox.com>
2014-11-15 11:57:07 -0500

    add deleted Cask eggscellent

---
+++ Casks/eggscellent.rb
@@ -0,0 +1,8 @@
+# Fri Jun 28 21:25:01 2013 +0200 https://github.com/caskroom/homebrew-cask/commit/0d44e5e Removes Eggscellent cask [Robert Curth]
+cask :v1 => 'eggscellent' do
+  url 'http://eggscellent.s3.amazonaws.com/betas/Eggscellent_Beta2.zip'
+  homepage 'http://www.eggscellentapp.com/'
+  version 'beta2'
+  sha256 '0fee4fb819fc51541322d671c4a6ca2829137f9f'
+  app 'Eggscellent.app'
+end
