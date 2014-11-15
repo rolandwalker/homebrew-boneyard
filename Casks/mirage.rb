5c3903999e89d695e2b03881afebfea6748e05ae
Roland Walker <walker@pobox.com>
2014-11-15 11:57:07 -0500

    add deleted Cask mirage

---
+++ Casks/mirage.rb
@@ -0,0 +1,8 @@
+# Wed Oct 2 15:08:40 2013 -0700 https://github.com/caskroom/homebrew-cask/commit/dc0a49d Remove Mirage [Fernando Paredes]
+cask :v1 => 'mirage' do
+  url 'http://cl.ly/1agC/download/Mirage%20101.zip'
+  homepage 'http://mac.softpedia.com/get/Wallpapers/Ferber-Mirage.shtml'
+  version :latest
+  sha256 :no_check
+  app 'Mirage.app'
+end
